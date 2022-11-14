adb8dd077ba4be7e2c09a12c7428ac03420cc4b4
HaiyiMei <haiyimei@mail.sdu.edu.cn>
2022-11-14 17:54:50 +0800

    add bp function of get feitoria plugin dirs

--- Content/Python/init_unreal.py
+++ Content/Python/init_unreal.py
@@ -1,29 +1,36 @@
 import unreal
+import GLOBAL_VARS
 
-@unreal.uenum()
-class MyPythonEnum(unreal.EnumBase):
-    FIRST = unreal.uvalue(0)
-    SECOND = unreal.uvalue(1)
-    FOURTH = unreal.uvalue(3)
+# @unreal.uenum()
+# class MyPythonEnum(unreal.EnumBase):
+#     FIRST = unreal.uvalue(0)
+#     SECOND = unreal.uvalue(1)
+#     FOURTH = unreal.uvalue(3)
+
+# @unreal.ustruct()
+# class PythonUnrealStruct(unreal.StructBase):
+#     some_string = unreal.uproperty(str)
+#     some_number = unreal.uproperty(float)
+#     array_of_string = unreal.uproperty(unreal.Array(str))
 
 @unreal.ustruct()
-class PythonUnrealStruct(unreal.StructBase):
-    some_string = unreal.uproperty(str)
-    some_number = unreal.uproperty(float)
-    array_of_string = unreal.uproperty(unreal.Array(str))
+class PathsStruct(unreal.StructBase):
+    project_dirs = unreal.uproperty(str)
+    feitoria_plugin_dirs = unreal.uproperty(str)
+    feitoria_plugin_python_dirs = unreal.uproperty(str)
 
 @unreal.uclass()
-class PythonTestClass(unreal.BlueprintFunctionLibrary):
+class XRFeitoriaBlueprintFunctionLibrary(unreal.BlueprintFunctionLibrary):
 
-    @unreal.ufunction(static = True, params = [int], ret = PythonUnrealStruct)
-    def MyPythonFunction(integer_argument1):
-        struct = PythonUnrealStruct()
-        struct.some_string = "5"
-        struct.some_number = integer_argument1 + 1
-        struct.array_of_string = ["a", "b", "c"]
-        return struct
+    @unreal.ufunction(static = True, params = [], ret = str, pure=True)
+    def GetPluginName():
+        return GLOBAL_VARS.PLUGIN_NAME
     
-    @unreal.ufunction(static = True, params = [str], ret = bool)
-    def MyPythonFunction2(string_argument1):
-        print(string_argument1)
-        return True
+    @unreal.ufunction(static = True, params = [], ret = PathsStruct, pure=True)
+    def GetFeitoriaPluginDirs():
+        PROJECT_ROOT, PLUGIN_ROOT, PLUGIN_PYTHON_ROOT, _ = GLOBAL_VARS.get_plugin_path()
+        struct = PathsStruct()
+        struct.project_dirs = str(PROJECT_ROOT)
+        struct.feitoria_plugin_dirs = str(PLUGIN_ROOT)
+        struct.feitoria_plugin_python_dirs = str(PLUGIN_PYTHON_ROOT)
+        return struct
