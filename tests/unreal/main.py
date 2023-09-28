f2e0dfce67a3a9dcac7629d200884c9d046c38af
Mei <haiyimei@mail.sdu.edu.cn>
2023-09-28 23:01:59 +0800

    Update main.py

--- tests/unreal/main.py
+++ tests/unreal/main.py
@@ -14,7 +14,7 @@ root = Path(__file__).parent
 
 def main(debug: bool = False, background: bool = False):
     logger = setup_logger(debug=debug, log_path=root / 'unreal.log')
-    with _init_unreal(background=background, dev_plugin=True) as xf_runner:
+    with _init_unreal(background=background) as xf_runner:
         init_test(debug=debug, background=background, dev=True)
         actor_test(debug=debug, background=background)
         camera_test(debug=debug, background=background)
