af5b5029189ff53c8ca9ee99cc550f43325855bb
HaiyiMei <haiyimei@mail.sdu.edu.cn>
2023-10-19 13:06:47 +0800

    typo

--- samples/blender/07_amass.py
+++ samples/blender/07_amass.py
@@ -27,30 +27,31 @@ def main():
     logger = Logger.setup_logging()
 
     # Download Amass from https://amass.is.tue.mpg.de/download.php
-    # For example, download ACCAD (SMPL-X N), and use any motion file from uncompressed folder
+    # For example, download ACCAD (SMPL-X N), and use any motion file from the uncompressed folder
     motion = load_amass_motion('ACCAD/s001/EricCamper04_stageii.npz')  # modify this to motion file in absolute path
     motion_data = motion.get_motion_data()
 
     xf_runner = xf.init_blender(
         exec_path='C:/Program Files/Blender Foundation/Blender 3.3/blender.exe'
     )  # modify this to your blender executable path
-    # Download SMPL-XL model from https://openxrlab-share.oss-cn-hongkong.aliyuncs.com/xrfeitoria/assets/SMPL-XL-001.fbx
+
+    # SMPL-XL model
+    # 1. Download SMPL-XL model from https://openxrlab-share.oss-cn-hongkong.aliyuncs.com/xrfeitoria/assets/SMPL-XL-001.fbx
     # or from https://openxlab.org.cn/datasets/OpenXDLab/SynBody/tree/main/Assets
     # With downloading this, you are agreeing to CC BY-NC-SA 4.0 License (https://creativecommons.org/licenses/by-nc-sa/4.0/)
-
-    # import SMPL-XL model
+    # 2. Import SMPL-XL model
     actor = xf_runner.Actor.import_from_file('SMPL-XL-001.fbx')  # modify this to SMPL-XL model file in absolute path
     apply_scale(actor.name)  # SMPL-XL model is imported with scale, we need to apply scale to it
     xf_runner.utils.apply_motion_data_to_actor(motion_data=motion_data, actor_name=actor.name)
 
-    # modify the frame range to the length of the motion
+    # Modify the frame range to the length of the motion
     frame_start, frame_end = xf_runner.utils.get_keys_range()
     xf_runner.utils.set_frame_range(frame_start, frame_end)
 
     logger.info('🎉 [bold green]Success!')
     input('Press Any Key to Exit...')
 
-    # close the blender process
+    # Close the blender process
     xf_runner.close()
 
 
