5f39031c6ea5ef923009f526a5fa3e644d506377
Haiyi <haiyimei@gmail.com>
2024-03-19 19:29:12 +0800

    [Version] Bump version to v0.6.1 (#27)

--- src/XRFeitoriaBpy/__init__.py
+++ src/XRFeitoriaBpy/__init__.py
@@ -4,11 +4,11 @@ from . import constants, operators, properties, ui  # isort:skip
 bl_info = {
     'name': 'XRFeitoriaBpy',
     'author': 'OpenXRLab',
-    'version': (0, 6, 0),
+    'version': (0, 6, 1),
     'blender': (3, 3, 0),
     'category': 'Tools',
 }
-__version__ = version = '0.6.0'
+__version__ = version = '0.6.1'
 
 
 def register():
