f4145bff77815a9ea88f99ae58c2731fda4ae90d
meihaiyi <haiyi.mei@qq.com>
2024-06-24 15:41:18 +0800

    update

--- .circleci/circleci_api.py
+++ .circleci/circleci_api.py
@@ -1,12 +1,12 @@
 import http.client
 
-conn = http.client.HTTPSConnection("circleci.com")
+conn = http.client.HTTPSConnection('circleci.com')
 
-headers = {"Circle-Token", "$CIRCLE_TOKEN"}
+headers = {'Circle-Token': '$CIRCLE_TOKEN'}
 
-conn.request("GET", "/api/v2/workflow/$CIRCLE_WORKFLOW_ID/job", headers=headers)
+conn.request('GET', '/api/v2/workflow/$CIRCLE_WORKFLOW_ID/job', headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 
-print(data.decode("utf-8"))
+print(data.decode('utf-8'))
