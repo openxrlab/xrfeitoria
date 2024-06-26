57087e6e311853a95d4dce3b528bfbd777f55a3d
Haiyi <haiyimei@gmail.com>
2024-06-26 17:54:13 +0800

    0.6.5 (#13)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/XRFeitoriaBpy/__init__.py
+++ src/XRFeitoriaBpy/__init__.py
@@ -4,11 +4,11 @@ from . import constants, operators, properties, ui  # isort:skip
 bl_info = {
     'name': 'XRFeitoriaBpy',
     'author': 'OpenXRLab',
-    'version': (0, 6, 4),  # XXX: Would be updated by CI/CD
+    'version': (0, 6, 5),  # XXX: Would be updated by CI/CD
     'blender': (3, 3, 0),
     'category': 'Tools',
 }
-__version__ = version = '0.6.4'
+__version__ = version = '0.6.5'
 
 
 def register():
