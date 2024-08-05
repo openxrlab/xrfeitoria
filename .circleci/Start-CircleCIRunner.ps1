1fc7e83555bd5547bdf34aeb3f7b43e92df29604
meihaiyi <haiyi.mei@qq.com>
2024-08-06 02:14:05 +0800

    chore: Update git clone path

--- .circleci/Start-CircleCIRunner.ps1
+++ .circleci/Start-CircleCIRunner.ps1
@@ -1,3 +1,5 @@
+# choco install -y git gzip tar
+
 $installDirPath = "D:\Program Files\CircleCI"
 $agentPath = Join-Path -Path $installDirPath -ChildPath "circleci-runner.exe"
 $configPath = Join-Path -Path $installDirPath -ChildPath "runner-agent-config.yaml"
